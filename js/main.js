8615f36e657b2a02284e6fd69e486f4e5713addb
Claire Kuang <kuang.claire@gmail.com>
2019-08-15 10:51:30 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('.navbar-link').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
+    });
+});
