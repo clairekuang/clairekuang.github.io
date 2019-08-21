7780db31555de19931f2dfc5f241f23dc945d0a6
Claire Kuang <kuang.claire@gmail.com>
2019-08-20 20:53:01 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var d = new Date();
+document.getElementById("year").innerHTML = d.getFullYear();
+document.getElementById("time").innerHTML = d.getHours().concat(":",d.getMinutes());
+
 $(function () {
     $('.navbar-link').on('click', function (e) {
         e.preventDefault();
