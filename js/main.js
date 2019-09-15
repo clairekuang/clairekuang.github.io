f3c04abf988c341805bb3b5f618f6371f6860a1c
clairekuang <kuang.claire@gmail.com>
2019-09-15 10:17:55 -0700

    line endings fixed

--- js/main.js
+++ js/main.js
@@ -2,9 +2,3 @@ var d = new Date();
 document.getElementById("year").innerHTML = d.getFullYear();
 document.getElementById("time").innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
 
-$(function () {
-    $('.navbar-link').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
-    });
-});
