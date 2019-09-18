f69157f9fbdfe57b6f72197e6259f5b1d8df8420
Claire Kuang <kuang.claire@gmail.com>
2019-09-17 21:34:14 -0700

    desktop project collapse

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ var d = new Date();
 document.getElementById("year").innerHTML = d.getFullYear();
 document.getElementById("time").innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
 
+
